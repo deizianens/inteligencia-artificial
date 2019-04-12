1e3fbe0d3316c70ef3283d1e9aba16a968db88bc
Deiziane <deiziane_ns@hotmail.com>
2019-04-12 19:10:17 -0300

    update bfs

--- TP1/board.py
+++ TP1/board.py
@@ -1,6 +1,9 @@
 import sys
+import time
+import resource
 import numpy as np
 from search import Search
+from state import State
 
 class Board:
     '''
@@ -16,34 +19,75 @@ class Board:
     def __init__(self, initial_values=[]):
         self.value = np.array(initial_values)
 
+
     def __eq__(self, other): 
         return self.value == other.value
 
 
-    def move_up(self, i):
-        if i - self.column_size >= 0:
-            puzzle_new, parent = self.swap(i, i - 3)
-            return Board(puzzle_new, parent, state='Up')
+    # caso em que a posição vazia não está na primeira linha (posso mover a posição vazia p/ cima)
+    def move_up(self):
+        pos = self.value.index(0)
+        if pos in (0, 1, 2):
+            return None
+        else:
+            new_val = list(self.value)
+            new_val[pos], new_val[pos-3] = new_val[pos-3], new_val[pos]
+            return new_val
 
-    def move_down(self, i):
-        if i + self.column_size <= len(self.puzzle_state) - 1:
-            puzzle_new, parent = self.swap(i, i + 3)
-            return Board(puzzle_new, parent, state='Down')
 
-    def move_left(self, i):
-        if i % self.column_size > 0:
-            puzzle_new, parent = self.swap(i, i - 1)
-            return Board(puzzle_new, parent, state='Left')
+    # caso em que a posição vazia não está na ultima linha (posso mover a posição vazia p/ baixo)
+    def move_down(self):
+        pos = self.value.index(0)
+        if pos in (6, 7, 8):
+            return None
+        else:
+            new_val = list(self.value)
+            new_val[pos], new_val[pos+3] = new_val[pos+3], new_val[pos]
+            return new_val
 
-    def move_right(self, i):
-        if i % self.column_size < self.column_size - 1:
-            puzzle_new, parent = self.swap(i, i + 1)
-            return Board(puzzle_new, parent, state='Right')
 
-    def swap(self, index_one, index_two):
-        puzzle_new = self.puzzle_state.copy()
-        puzzle_new[index_one], puzzle_new[index_two] = puzzle_new[index_two], puzzle_new[index_one]
-        return puzzle_new, self
+     # caso em que a posição vazia não está na coluna da esquerda (posso mover a posição vazia p/ esquerda)
+    def move_left(self):
+        pos = self.value.index(0)
+        if pos in (0, 3, 6):
+            return None
+        else:
+            new_val = list(self.value)
+            new_val[pos], new_val[pos-1] = new_val[pos-1], new_val[pos]
+            return new_val
+
+
+    # caso em que a posição vazia não está na coluna da direita (posso mover a posição vazia p/ direita)
+    def move_right(self):
+        pos = self.value.index(0)
+        if pos in (2, 5, 8):
+            return None
+        else:
+            new_val = list(self.value)
+            new_val[pos], new_val[pos+1] = new_val[pos+1], new_val[pos]
+            return new_val
+
+
+def trace_path(last_pos):
+    pos = last_pos.prev
+    next_pos = last_pos
+
+    path = []
+
+    while pos != None:
+        if pos.node.up() == next_pos.node:
+            path.append("Up")
+        elif pos.node.down() == next_pos.node:
+            path.append("Down")
+        elif pos.node.left() == next_pos.node:
+            path.append("Left")
+        elif pos.node.right() == next_pos.node:
+            path.append("Right")
+
+        pos = pos.prev
+        next_pos = next_pos.prev
+
+    return path[::-1]
 
 
 def main():
@@ -51,7 +95,26 @@ def main():
     puzzle_state = list(map(int, puzzle_state)) # transforma tudo em int
     pb = Board(puzzle_state)
 
+    start_time = time.time()
+
     search_depth, states, max_depth, nodes_expanded = Search().bfs(pb)
+
+    config = [1,2,3,0,4,5,6,7,8]
+
+    game = State(config)
+
+    result = search.bfs(game)
+    final_pos = result.position
+    max_depth = result.max_depth
+    nodes_expanded = result.nodes_expanded
+
+    print ("path_to_goal:", trace_path(final_pos))
+    print ("cost_of_path:", final_pos.cost)
+    print ("nodes_expanded:", nodes_expanded)
+    print ("search_depth:", final_pos.depth)
+    print ("max_search_depth:", max_depth)
+    print ("running_time:", time.time() - start_time)
+    print ("max_ram_usage", resource.getrusage(1)[2])
 
 
 if __name__ == '__main__':
